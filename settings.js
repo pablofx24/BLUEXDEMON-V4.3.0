e0e66959bc1f31380c5094a898e1ae2d5432e91c
pablofx24 <pablofx634@gmail.com>
2025-01-10 23:44:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,19 +1,19 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "2349036325074"
+global.pairingNumber = "2348137453915"
 
-global.botNumber = ["2349036325074"]
+global.botNumber = ["2348137453915"]
 global.domain = "https://.nobodysey.me"
 global.apikey = "ptla_DfGlmoY7jCzmkNUgyK925FPVE08Kr4mxwLMep"
 global.capikey = "ptlc_GTm42E3qIwbLW3K9w3htljUvhGvnG71hoPTqE"
 global.egg = "15"
 global.loc = "1"
 global.anticall = false
-global.autoReadChat = false
+global.autoReadChat = true
 global.alwaysonline = true
-global.autoswview = false
+global.autoswview = true
 global.public = true
-global.autoreact = false
+global.autoreact = true
 global.antidelete = false
 global.autotyping = false
 global.autoBio = true
